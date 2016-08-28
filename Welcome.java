7ed90ea9b9c308de568d9b79dfe345b608de1f10
gssakib <gssakib>
2016-08-28 14:05:46 -0400

    chap 1 stuff

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,17 @@
 public class Welcome {
 	
-	public static void main(String[] args) {
+	/*  public static void main(String[] args) {
 		
 		System.out.println("Welcome to JAVA!");
+		System.out.println("Life is good....");
+		System.out.println("NA NA NA, wait till i get my  money right....");
+		
+	}  */
+	public static void main(String[] args) {
+		
+		System.out.println("3.5 * 4 / 2 - 2.5 is =");
+		System.out.println(((3.5 * 4) / 2) - 2.5);
+		
 		
 	}
 	
