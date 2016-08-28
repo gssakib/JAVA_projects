4efcd9f7850a4cd8e78a90128d144e5060bb99a7
gssakib <gssakib>
2016-08-28 19:42:55 -0400

    new practice

---
+++ demo/demo/displays.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class displays {
+
+	public static void main(String[] args) {
+		
+	/*  System.out.println("Welcome to JAVA!!");
+		System.out.println("Welcome to Comp Sci!!");
+		System.out.println("Programming is fun!!");
+	*/
+		// TODO Auto-generated method stub
+
+		System.out.println("    J    A      V     V     A");
+		System.out.println("    J   A A      V   V     A A");
+		System.out.println("J   J  AAAAA      V V     AAAAA");
+		System.out.println(" J J  A     A      V     A     A");
+		
+	}
+
+}
