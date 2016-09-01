fcc96f887252ee1c3145671f8321e28570b9b37a
gssakib <gssakib>
2016-08-31 23:41:52 -0400

    almost done with chap 2

--- demo/demo/Test.java
+++ demo/demo/Test.java
@@ -40,8 +40,9 @@ public class Test {
 		System.out.println("The value of the epxression is: " + exp);
 		
 		
-		*/
-	
+		
+
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter value for r: ");
 		
@@ -55,8 +56,73 @@ public class Test {
 		System.out.println("The value of the expression is: " + exp);
 		
 		
+		*/
+		
+		/*
+		double  x = 2;
+		x += 2;
+		System.out.println(x += 2);
+		*/
+		
+		/*
+		double a = 6.5;
+		a += a + 1;
+		
+		System.out.print(a);
+		
+		double b = 6;
+		b /= 2;
+		System.out.println(b);
 		
 		
+		int i = 2;
+		int k = ++i + i ;
+		
+	System.out.println("k:" + k);
+		*/
+		/*
+		
+		int x = 0;
+		int y = 0;
+		x = y = x = 1;
+		System.out.println("x :" + x);
+		System.out.println("y :" + y);
+	
+	
+		int a = 6;
+		int b = a++;
+		System.out.println(a);
+		System.out.println(b);
+		
+		a = 6;
+		b = ++a;
+		System.out.println(a);
+		System.out.println(b);
+		
+	
+		
+		int sum = 0;
+		sum = (int)(sum + 4.5);
+		System.out.println(sum);
+		
+	
+		
+		float f = 12.5F;
+		int i = (int)f;
+		System.out.println("f is " + f);
+		System.out.println("i is " + i);
+		
+	*/
+		double amount = 5;
+		System.out.println(amount / 2);
+		System.out.println(5 / 2);
+		
+		
+		
+		
+		
+	
+	
 	}
 
 }
